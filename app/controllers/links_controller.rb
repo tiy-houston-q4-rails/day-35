470a51a17b216b870d8b7866bb56155ce2bc7577
Jesse Wolgamott <jesse@comalproductions.com>
2014-11-07 15:02:09 -0600

    SPA'ify it

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,18 +1,13 @@
 class LinksController < ApplicationController
   def index
     @links = Link.all
-  end
-
-  def new
     @link = Link.new
   end
 
   def create
     @link = Link.new(params.require(:link).permit(:url, :photo, :notes))
-    if @link.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    @link.save
+    @links = Link.all
+    @link = Link.new
   end
 end
